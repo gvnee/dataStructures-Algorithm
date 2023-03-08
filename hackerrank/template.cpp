47818bcb03624eb27b927a81284b66018b79f215
gvnee <usopp.plazma@yahoo.com>
2023-03-08 12:46:16 +0800

    template

--- hackerrank/template.cpp
+++ hackerrank/template.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
+using ll = long long;
 int main(){
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
   #ifndef ONLINE_JUDGE
   freopen("input.txt", "r", stdin);
   #endif
