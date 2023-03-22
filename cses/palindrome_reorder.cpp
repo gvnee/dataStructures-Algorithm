6b7b6c6fe17147053c9eebf62ca55c9eeba8d4ed
gvnee <usopp.plazma@yahoo.com>
2023-03-22 23:56:20 +0800

    palindrome reordergit add cses!

--- cses/palindrome_reorder.cpp
+++ cses/palindrome_reorder.cpp
@@ -10,6 +10,41 @@ double PI = atan(1)*4;
 int temp;
 
 void f(){
+  string str;
+  cin>>str;
+  int freq[26] = {};
+  for(int i = 0;i<str.size();i++){
+    freq[str[i] - 'A']++;
+  }
+  // for(int i = 0;i<26;i++) cout<<freq[i]<<" ";
+  int odd = 0;
+  int oddIndex;
+  for(int i = 0;i<26;i++){
+    if(freq[i]%2==1){
+      odd++;
+      oddIndex = i;
+    }
+    if(odd > 1){
+      cout<<"NO SOLUTION\n";
+      return;
+    }
+  }
+
+  deque<int> deq;
+  if(odd == 1){
+    for(int i = 0;i<freq[oddIndex];i++){
+      deq.push_back(oddIndex);
+    }
+  }
+  for(int i = 0;i<26;i++){
+    if(odd == 1 && oddIndex == i) continue;
+    for(int j = 0;j<freq[i]/2;j++){
+      deq.push_back(i);
+      deq.push_front(i);
+    }
+  }
+
+  for(auto it:deq) cout<<char(it + 'A');
 
 }
 
