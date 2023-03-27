efb350134ba35e2e77da0458d2053f6a7e57476a
gvnee <usopp.plazma@yahoo.com>
2023-03-27 16:33:39 +0800

    hackerrank

---
+++ hackerrank/ustgal.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define debug(x) cerr<<#x<<": "<<(x)<<"\n"
+
+const int MOD = int(1e9) + 7;
+double PI = atan(1)*4;
+int temp;
+
+void f(){
+  unsigned long long n, k;
+  cin>>n>>k;
+  int i = 0;
+  while(true){
+    i++;
+    if(n%k == 0){
+      cout<<i<<"\n";
+      return;
+    }
+    n -= n/k;
+  }
+}
+
+int main(){
+  
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+  #ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+  #endif
+
+  int t = 1;
+  // cin>>t;
+  while(t--) f();
+  
+  return 0;
+}
