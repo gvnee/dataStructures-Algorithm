71ffb463613366f77f368df2e5590b973e848255
gvnee <usopp.plazma@yahoo.com>
2023-03-28 23:40:33 +0800

    kattis

---
+++ kattis/basketballoneonone.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define debug(x) cerr<<#x<<": "<<(x)<<"\n"
+
+const int MOD = int(1e9) + 7;
+double PI = atan(1)*4;
+int temp;
+
+void f(){
+  string s;
+  cin>>s;
+  cout<<s[s.size()-2];
+}
+
+int main(){
+  
+  ios_base::sync_with_stdio(false); cin.tie(nullptr);
+  #ifndef ONLINE_JUDGE
+    freopen("input.in", "r", stdin);
+  #endif
+
+  int t = 1;
+  // cin>>t;
+  while(t--) f();
+  
+  return 0;
+}
