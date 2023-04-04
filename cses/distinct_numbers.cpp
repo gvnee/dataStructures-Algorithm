14cbddc718faea85b90901ff4dca90e079922167
gvnee <usopp.plazma@yahoo.com>
2023-04-04 22:25:51 +0800

    cses

--- cses/distinct_numbers.cpp
+++ cses/distinct_numbers.cpp
@@ -4,25 +4,29 @@ using ll = long long;
 
 const int MOD = int(1e9) + 7;
 double PI = atan(1)*4;
+#define debug(x) cerr<<#x<<": "<<(x)<<"\n"
 int temp;
 
 void f(){
   int n;
   cin>>n;
-  unordered_set<int> s;
+  int arr[n];
+  for(int i = 0;i<n;i++) cin>>arr[i];
+  sort(arr, arr+n);
+  int cur = -1;
+  int res = 0;
   for(int i = 0;i<n;i++){
-    cin>>temp;
-    s.insert(temp);
+    if(arr[i] != cur){
+      cur = arr[i];
+      res++;
+    }
   }
-  cout<<s.size();
+  cout<<res<<"\n";
 }
 
 int main(){
   
   ios_base::sync_with_stdio(false); cin.tie(nullptr);
-  #ifndef ONLINE_JUDGE
-    freopen("input.in", "r", stdin);
-  #endif
 
   int t = 1;
   // cin>>t;
