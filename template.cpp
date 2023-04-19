f351379ba55b68289e77b569e6cb7a50ead02d48
gvnee <usopp.plazma@yahoo.com>
2023-04-19 18:08:34 +0800

    template

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define sz(x) int((x).size())
+#define debug(x) cerr<<#x<<": "<<(x)<<"\n"
+#define pb push_back
+
+void f(){
+
+}
+
+int main(){
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  int t = 1;
+  // cin>>t;
+  while(t--) f();
+  return 0;
+}
